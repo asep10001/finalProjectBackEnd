d3e85f18a4133a02f115e44ad0873384e3b47315
asep10001 <asep10001@mail.unpad.ac.id>
2020-12-22 22:22:39 +0700

    adding income create

--- src/main/java/com/asep1001/finalprojectudacoding/controllers/IncomeController.java
+++ src/main/java/com/asep1001/finalprojectudacoding/controllers/IncomeController.java
@@ -1,11 +1,14 @@
 package com.asep1001.finalprojectudacoding.controllers;
 
+import com.asep1001.finalprojectudacoding.model.Category;
+import com.asep1001.finalprojectudacoding.model.Income;
 import com.asep1001.finalprojectudacoding.services.IncomeService;
+import com.asep1001.finalprojectudacoding.services.dto.CategoryDTO;
 import com.asep1001.finalprojectudacoding.services.dto.IncomeDTO;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -19,5 +22,12 @@ public class IncomeController {
     @GetMapping("/incomes")
     public ResponseEntity<List<IncomeDTO>> getAllIncomes(){
         return incomeService.getAllIncomes();
+    }
+
+    @PostMapping("/incomes")
+    public ResponseEntity<IncomeDTO> saveIncome(
+            @Valid @RequestBody Income request, @RequestParam(value = "categoryId") Long id
+    ) {
+        return incomeService.createAnIncome(request, id);
     }
 }
--- src/main/java/com/asep1001/finalprojectudacoding/services/IncomeService.java
+++ src/main/java/com/asep1001/finalprojectudacoding/services/IncomeService.java
@@ -1,6 +1,8 @@
 package com.asep1001.finalprojectudacoding.services;
 
+import com.asep1001.finalprojectudacoding.model.Category;
 import com.asep1001.finalprojectudacoding.model.Income;
+import com.asep1001.finalprojectudacoding.repository.CategoryRepostitory;
 import com.asep1001.finalprojectudacoding.repository.IncomeRepository;
 import com.asep1001.finalprojectudacoding.services.dto.IncomeDTO;
 import com.asep1001.finalprojectudacoding.services.mapper.IncomeMapper;
@@ -15,10 +17,12 @@ import java.util.function.Function;
 public class IncomeService {
 
     private final IncomeRepository incomeRepository;
+    private final CategoryRepostitory categoryRepostitory;
 
 
-    public IncomeService(IncomeRepository incomeRepository) {
+    public IncomeService(IncomeRepository incomeRepository, CategoryRepostitory categoryRepostitory) {
         this.incomeRepository = incomeRepository;
+        this.categoryRepostitory = categoryRepostitory;
     }
 
     private Function<List<Income>, List<IncomeDTO>> toDtos() {
@@ -29,11 +33,31 @@ public class IncomeService {
         return (x) -> new ResponseEntity<>(this.toDtos().apply(x), HttpStatus.OK);
     }
 
+    private Function<Income, ResponseEntity<IncomeDTO>> getAnIncome() {
+        return (x) -> new ResponseEntity<>(this.toDto().apply(x), HttpStatus.OK);
+    }
+
     private Function<Income, IncomeDTO> toDto() {
         return (x) -> IncomeMapper.INSTANCE.toDto(x);
     }
 
     public ResponseEntity<List<IncomeDTO>> getAllIncomes() {
         return this.getAll().apply(incomeRepository.findAll());
+    }
+
+    public ResponseEntity<IncomeDTO> createAnIncome(Income request, Long categoryId) {
+
+        Category cEntity = categoryRepostitory.findById(categoryId).get();
+        Income iEntity = Income.builder()
+                .name(request.getName())
+                .ammount(request.getAmmount())
+                .transaction_date(request.getTransaction_date())
+                .category(cEntity)
+                .build();
+
+        incomeRepository.save(iEntity);
+
+        return this.getAnIncome().apply(iEntity);
+
     }
 }
